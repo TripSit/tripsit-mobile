076a806cd8f7749aeaeef6e1273708957002ee91
Moonbear <teknos@tripsit.me>
2022-04-25 13:48:22 -0500

    Updated combo to include the combo definition

--- modules/combo.js
+++ modules/combo.js
@@ -1,14 +1,17 @@
-import * as React from 'react';
+import * as React from "react";
 import {
   SafeAreaView,
   StyleSheet,
   Text,
   View,
   TouchableOpacity,
-} from "react-native"
-
-import * as allDrugData from '../assets/allDrugData.json';
-import SearchableDropdown from 'react-native-searchable-dropdown';
+} from "react-native";
+import * as allDrugData from "../assets/allDrugData.json";
+import * as combo_definitions from "../assets/combo_definitions.json";
+import SearchableDropdown from "react-native-searchable-dropdown";
+// const logger = require('./utils/logger.js');
+// const PREFIX = require('path').parse(__filename).name;
+const PREFIX = "combo";
 
 // For each dictionary in the allDrugData list, find the "name" key and add it to a list of index-title pairs
 const drugNames = Object.keys(allDrugData).map((key, index) => {
@@ -16,16 +19,15 @@ const drugNames = Object.keys(allDrugData).map((key, index) => {
   // console.log(allDrugData[key]["summary"])
   return {
     id: index,
-    name: allDrugData[key]["name"]
-  }
-})
-
+    name: allDrugData[key]["name"],
+  };
+});
 
 // Okay this is stupid, but I'm not sure how to do it better
 // We pop out the last element of the array, because the above MAP function adds an 'undefined' element to the array
 // The above function adds an undefined element because that's how the JSON file is imported, for some reason
 // TODO - JSON UNDEFINED ISSUE
-drugNames.pop()
+drugNames.pop();
 
 // console.log(drugNames)
 var drug_a = "";
@@ -33,52 +35,79 @@ var drug_b = "";
 
 function ComboScreen() {
   // Define objects to hold search data
-  const [drugA, setDrugA] = React.useState('');
-  const [drugB, setDrugB] = React.useState('');
+  const [drugA, setDrugA] = React.useState("");
+  const [drugB, setDrugB] = React.useState("");
   // Much like Factsheets, the results only display after you search
   const [shouldShow, setShouldShow] = React.useState(false);
   // Define object to store results
   const [results, setResults] = React.useState({});
 
-
   function getDrugCombo() {
     // console.log("started combo!")
     // console.log("drug_a is: " + drug_a)
     // console.log("drug_b is: " + drug_b)
-    var comboData = ""
+    var comboData = "";
     if (drug_a != "" && drug_b != "") {
-      // console.log("both drugs are set!")
+      console.log("both drugs are set!");
       for (var i = 0; i < Object.keys(allDrugData).length; i++) {
         // TODO - JSON UNDEFINED ISSUE - Same as above
         if (!!allDrugData[i]) {
           // console.debug("allDrugData[i]['name'] is: " + allDrugData[i]['name'])
-          if (allDrugData[i]['name'] == drug_a) {
-            // console.debug("found drug_a: " + drug_a)
-            drugData = allDrugData[i]
-            if (!!allDrugData[i]['interactions']) {
-              // console.debug("drug_a has interactions!")
-              // console.debug("drugData[i]['interactions'] is: " + allDrugData[i]['interactions'])
-              for (var j = 0; j < allDrugData[i]['interactions'].length; j++) {
-                // console.debug("j is: " + j)
-                // console.debug("allDrugData[i]['interactions'][j]['name'] is: " + allDrugData[i]['interactions'][j]['name'])
-                if (allDrugData[i]['interactions'][j]['name'] == drug_b) {
-                  // console.debug("found drug_b: " + drug_b)
-                  comboData = allDrugData[i]['interactions'][j]['status']
-                  // console.debug("comboData is: " + comboData)
-                  setResults(comboData)
-                  setShouldShow(true)
-                  break
+          if (allDrugData[i]["name"] == drug_a) {
+            console.debug("found drug_a: " + drug_a);
+            drugData = allDrugData[i];
+            if (!!allDrugData[i]["interactions"]) {
+              console.debug(
+                `${drug_a} interactions: ${allDrugData[i]["interactions"].length}`
+              );
+              for (var j = 0; j < allDrugData[i]["interactions"].length; j++) {
+                console.debug(
+                  `Interaction ${j} is: ${allDrugData[i]["interactions"][j]["name"]}`
+                );
+                if (allDrugData[i]["interactions"][j]["name"] == drug_b) {
+                  const status = allDrugData[i]["interactions"][j]["status"];
+                  console.debug(`Found interaction with ${drug_b}: ${status}`);
+                  // Loop through combo_definitions and find the object where "status" is equal to result
+                  for (
+                    var k = 0;
+                    k < Object.keys(combo_definitions).length;
+                    k++
+                  ) {
+                    if (combo_definitions[k]["status"] == status) {
+                      console.debug(
+                        `Found combo_defs: ${combo_definitions[k]["status"]}`
+                      );
+                      const definition = combo_definitions[k]["definition"];
+                      console.debug(`Definition: ${definition}`);
+                      const emoji = combo_definitions[k]["emoji"];
+                      console.debug(`Emoji: ${emoji}`);
+                      const color = combo_definitions[k]["color"];
+                      console.debug(`Color: ${color}`);
+                      const output = `${emoji}  ${status} ${emoji}`;
+                      comboData = `${output}\n\n${definition}`;
+                      console.debug(`Output: ${output}`);
+                      setResults(comboData);
+                      setShouldShow(true);
+                      break;
+                    }
+                  }
+                  break;
                 }
               }
               if (comboData == "") {
-                setResults("Interaction data not found between " + drug_a + " and " + drug_b)
-                setShouldShow(true)
-                break
+                setResults(
+                  "Interaction data not found between " +
+                    drug_a +
+                    " and " +
+                    drug_b
+                );
+                setShouldShow(true);
+                break;
               }
             } else {
-              setResults("Interaction data not found for " + drug_a)
-              setShouldShow(true)
-              break
+              setResults("Interaction data not found for " + drug_a);
+              setShouldShow(true);
+              break;
             }
           }
         }
@@ -90,98 +119,86 @@ function ComboScreen() {
     <SafeAreaView style={[{ flex: 1 }]}>
       <View style={styles.container}>
         <View style={styles.row}>
-        <View>
+          <View>
             <SearchableDropdown
               onItemSelect={(item) => {
                 // console.log(item.name)
-                drug_a = item.name
-                setDrugA(item.name)
-                getDrugCombo()
+                drug_a = item.name;
+                setDrugA(item.name);
+                getDrugCombo();
               }}
               containerStyle={{ padding: 5 }}
               itemStyle={{
                 padding: 10,
                 marginTop: 2,
-                backgroundColor: '#ddd',
-                borderColor: '#bbb',
+                backgroundColor: "#ddd",
+                borderColor: "#bbb",
                 borderWidth: 1,
                 borderRadius: 5,
               }}
-              itemTextStyle={{ color: '#222' }}
+              itemTextStyle={{ color: "#222" }}
               itemsContainerStyle={{ maxHeight: 140 }}
               items={drugNames}
               resetValue={false}
-              textInputProps={
-                {
-                  placeholder: "Search substance here!",
-                  underlineColorAndroid: "transparent",
-                  style: {
-                      padding: 12,
-                      borderWidth: 1,
-                      borderColor: '#ccc',
-                      borderRadius: 5,
-                      backgroundColor: '#ffffff'
-                  },
-                }
-              }
-              listProps={
-                {
-                  nestedScrollEnabled: true,
-                }
-              }
+              textInputProps={{
+                placeholder: "Search substance here!",
+                underlineColorAndroid: "transparent",
+                style: {
+                  padding: 12,
+                  borderWidth: 1,
+                  borderColor: "#ccc",
+                  borderRadius: 5,
+                  backgroundColor: "#ffffff",
+                },
+              }}
+              listProps={{
+                nestedScrollEnabled: true,
+              }}
             />
           </View>
           <View>
             <SearchableDropdown
               onItemSelect={(item) => {
                 // console.log(item.name)
-                drug_b = item.name
-                setDrugB(item.name)
-                getDrugCombo()
+                drug_b = item.name;
+                setDrugB(item.name);
+                getDrugCombo();
               }}
               containerStyle={{ padding: 5 }}
               itemStyle={{
                 padding: 10,
                 marginTop: 2,
-                backgroundColor: '#ddd',
-                borderColor: '#bbb',
+                backgroundColor: "#ddd",
+                borderColor: "#bbb",
                 borderWidth: 1,
                 borderRadius: 5,
               }}
-              itemTextStyle={{ color: '#222' }}
+              itemTextStyle={{ color: "#222" }}
               itemsContainerStyle={{ maxHeight: 140 }}
               items={drugNames}
               resetValue={false}
-              textInputProps={
-                {
-                  placeholder: "Search substance here!",
-                  underlineColorAndroid: "transparent",
-                  style: {
-                      padding: 12,
-                      borderWidth: 1,
-                      borderColor: '#ccc',
-                      borderRadius: 5,
-                      backgroundColor: '#ffffff'
-                  },
-                }
-              }
-              listProps={
-                {
-                  nestedScrollEnabled: true,
-                }
-              }
+              textInputProps={{
+                placeholder: "Search substance here!",
+                underlineColorAndroid: "transparent",
+                style: {
+                  padding: 12,
+                  borderWidth: 1,
+                  borderColor: "#ccc",
+                  borderRadius: 5,
+                  backgroundColor: "#ffffff",
+                },
+              }}
+              listProps={{
+                nestedScrollEnabled: true,
+              }}
             />
           </View>
         </View>
         <View style={styles.row}>
-          <TouchableOpacity
-            style={styles.button}
-          >
+          <TouchableOpacity style={styles.button}>
             <Text style={styles.buttonText}>{drugA}</Text>
           </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.button}
-          >
+          <TouchableOpacity style={styles.button}>
             <Text style={styles.buttonText}>{drugB}</Text>
           </TouchableOpacity>
         </View>
@@ -192,27 +209,27 @@ function ComboScreen() {
         ) : null}
       </View>
     </SafeAreaView>
-  )
+  );
 }
 
 const color_dict = {
-  black: '#000000',
-  red: '#FF0000',
-  green: '#00FF00',
-  blue: '#0000FF',
-  white: '#FFFFFF',
-  purple: '#4B0082',
+  black: "#000000",
+  red: "#FF0000",
+  green: "#00FF00",
+  blue: "#0000FF",
+  white: "#FFFFFF",
+  purple: "#4B0082",
 };
 
 const styles = StyleSheet.create({
   flex: {
-    flex: 1
+    flex: 1,
   },
   justifyCenter: {
-    justifyContent: 'center'
+    justifyContent: "center",
   },
   row: {
-    flexDirection: 'row',
+    flexDirection: "row",
   },
   input: {
     height: 50,
@@ -224,7 +241,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: color_dict.black,
-    alignItems: 'center',
+    alignItems: "center",
     // padding: 20,
   },
   logo: {
@@ -233,7 +250,7 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   button: {
-    width: '50%',
+    width: "50%",
     padding: 20,
     borderRadius: 5,
   },
@@ -242,22 +259,22 @@ const styles = StyleSheet.create({
     padding: 20,
     borderRadius: 5,
     height: 10,
-    justifyContent: 'space-around'
+    justifyContent: "space-around",
   },
   doubleButton: {
-    width: '100%',
+    width: "100%",
     padding: 20,
     borderRadius: 5,
   },
   buttonText: {
     fontSize: 20,
     color: color_dict.white,
-    alignSelf: 'center',
+    alignSelf: "center",
   },
   icon: {
     height: 60,
     width: 60,
-    alignSelf: 'center',
+    alignSelf: "center",
   },
   text: {
     fontSize: 16,
@@ -270,12 +287,12 @@ const styles = StyleSheet.create({
   aboutHeaderText: {
     fontSize: 20,
     color: color_dict.white,
-    alignSelf: 'center'
+    alignSelf: "center",
   },
   headerText: {
-    textAlign: 'center',
+    textAlign: "center",
     fontSize: 16,
-    fontWeight: '500',
+    fontWeight: "500",
     color: color_dict.white,
   },
   content: {
@@ -288,12 +305,12 @@ const styles = StyleSheet.create({
     backgroundColor: color_dict.black,
   },
   scrollContainer: {
-    flex: 1
+    flex: 1,
   },
   title: {
     textAlign: "center",
     fontSize: 25,
-    marginBottom: 50
+    marginBottom: 50,
   },
   sectionTitle: {
     fontWeight: "bold",
