b7a198c066ef0ce5235b1dd94d1a65524e047d2c
Daniel Moret <daniel.moret@cmbr.co>
2022-05-08 14:47:39 -0600

    make combo status stand out

--- modules/combo.js
+++ modules/combo.js
@@ -46,23 +46,28 @@ function ComboScreen() {
     // console.log("started combo!")
     // console.log("drug_a is: " + drug_a)
     // console.log("drug_b is: " + drug_b)
-    var comboData = "";
+    let comboData = false;
     if (drug_a != "" && drug_b != "") {
-    // console.log("both drugs are set!");
+      // console.log("both drugs are set!");
+
+      console.log("combo_definitions :: ", combo_definitions);
+      console.log("allDrugData :: ", allDrugData);
+      console.log("drug_a :: ", drug_a);
+      console.log("drug_b :: ", drug_b);
       for (var i = 0; i < Object.keys(allDrugData).length; i++) {
         // TODO - JSON UNDEFINED ISSUE - Same as above
         if (!!allDrugData[i]) {
           // console.debug("allDrugData[i]['name'] is: " + allDrugData[i]['name'])
           if (allDrugData[i]["name"] == drug_a) {
-          // console.debug("found drug_a: " + drug_a);
-            drugData = allDrugData[i];
+            // console.debug("found drug_a: " + drug_a);
+            //drugData = allDrugData[i];
             if (!!allDrugData[i]["interactions"]) {
-            // console.debug(`${drug_a} interactions: ${allDrugData[i]["interactions"].length}`);
+              // console.debug(`${drug_a} interactions: ${allDrugData[i]["interactions"].length}`);
               for (var j = 0; j < allDrugData[i]["interactions"].length; j++) {
-              // console.debug(`Interaction ${j} is: ${allDrugData[i]["interactions"][j]["name"]}`);
+                // console.debug(`Interaction ${j} is: ${allDrugData[i]["interactions"][j]["name"]}`);
                 if (allDrugData[i]["interactions"][j]["name"] == drug_b) {
                   const status = allDrugData[i]["interactions"][j]["status"];
-                // console.debug(`Found interaction with ${drug_b}: ${status}`);
+                  // console.debug(`Found interaction with ${drug_b}: ${status}`);
                   // Loop through combo_definitions and find the object where "status" is equal to result
                   for (
                     var k = 0;
@@ -70,17 +75,21 @@ function ComboScreen() {
                     k++
                   ) {
                     if (combo_definitions[k]["status"] == status) {
-                    // console.debug(`Found combo_defs: ${combo_definitions[k]["status"]}`);
+                      // console.debug(`Found combo_defs: ${combo_definitions[k]["status"]}`);
                       const definition = combo_definitions[k]["definition"];
-                    // console.debug(`Definition: ${definition}`);
                       const emoji = combo_definitions[k]["emoji"];
-                    // console.debug(`Emoji: ${emoji}`);
+                      const svg = combo_definitions[k]["svg"];
                       const color = combo_definitions[k]["color"];
-                    // console.debug(`Color: ${color}`);
-                      const output = `${emoji}  ${status} ${emoji}`;
-                      comboData = `${output}\n\n${definition}`;
-                    // console.debug(`Output: ${output}`);
-                      setResults(comboData);
+                      const title = `${emoji} ${status} ${emoji}`;
+
+                      comboData = true;
+
+                      setResults({
+                        title: title,
+                        definition: definition,
+                        color: color,
+                        color_code: color_dict[color.toLowerCase()],
+                      });
                       setShouldShow(true);
                       break;
                     }
@@ -88,18 +97,18 @@ function ComboScreen() {
                   break;
                 }
               }
-              if (comboData == "") {
-                setResults(
-                  "Interaction data not found between " +
-                    drug_a +
-                    " and " +
-                    drug_b
-                );
+
+              if (!comboData) {
+                setResults({
+                  error: `Interaction data not found between ${drug_a} and ${drug_b}`,
+                });
                 setShouldShow(true);
                 break;
               }
             } else {
-              setResults("Interaction data not found for " + drug_a);
+              setResults({
+                error: `Interaction data not found for ${drug_a}`,
+              });
               setShouldShow(true);
               break;
             }
@@ -108,6 +117,16 @@ function ComboScreen() {
       }
     }
   }
+
+  const comboTitleStyle = function (color_code) {
+    return {
+      fontSize: 20,
+      color: color_dict.white,
+      alignSelf: "center",
+      backgroundColor: color_code,
+      padding: 5,
+    };
+  };
 
   return (
     <SafeAreaView style={[{ flex: 1 }]}>
@@ -196,23 +215,36 @@ function ComboScreen() {
             <Text style={styles.buttonText}>{drugB}</Text>
           </TouchableOpacity>
         </View>
-        {shouldShow ? (
-          <View style={styles.container}>
-            <Text style={styles.buttonText}>{results}</Text>
-          </View>
-        ) : null}
+        {console.log("shouldShow :: ", shouldShow)}
+        {console.log("results :: ", results)}
+        <View style={styles.container}>
+          {!results.error ? (
+            <>
+              <Text style={comboTitleStyle(results.color_code)}>
+                {results.title}
+              </Text>
+              <Text style={styles.comboDefineStyle}>{results.definition}</Text>
+            </>
+          ) : (
+            <Text style={styles.comboDefineStyle}>{results.error}</Text>
+          )}
+        </View>
       </View>
     </SafeAreaView>
   );
 }
 
 const color_dict = {
-  black: "#000000",
   red: "#FF0000",
+  orange: "#D98427",
+  //yellow: "#D5C625",
   green: "#00FF00",
-  blue: "#0000FF",
-  white: "#FFFFFF",
+  aqua: "#006CB3",
+  blue: "#35AFFF",
+  dark_blue: "#0A89DD",
   purple: "#4B0082",
+  black: "#000000",
+  white: "#FFFFFF",
 };
 
 const styles = StyleSheet.create({
@@ -312,6 +344,13 @@ const styles = StyleSheet.create({
   },
   section: {
     marginBottom: 40,
+  },
+  comboDefineStyle: {
+    fontSize: 20,
+    color: color_dict.white,
+    alignSelf: "center",
+    padding: 26,
+    paddingTop: 15,
   },
 });
 
